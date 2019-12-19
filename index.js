c7d3bcd638c237efcfb630171423e6d3a3827938
Yingqi Chen <chanwk18@hotmail.com>
2019-12-19 00:23:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books')
-  .then(resp=>resp.json())
-  .then(json=>renderBooks(json))
-}
-
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.numberOfPages}</h2>`
-    main.appendChild(h2)
-  })
-}
+// function fetchBooks() {
+//   return fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp=>resp.json())
+//   .then(json=>renderBooks(json))
+// }
+// 
+// function renderBooks(json) {
+//   const main = document.querySelector('main')
+//   json.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = `<h2>${book.numberOfPages}</h2>`
+//     main.appendChild(h2)
+//   })
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
   //  fetchBooks()
   fetchCharacter1031()
 })
 
-// function fetchCharacter1031(){
-//   return fetch('https://anapioficeandfire.com/api/characters/1031')
-//   .then(resp=>resp.json())
-//   .then(json=>console.log(json))
-// }
+function fetchCharacter1031(){
+  return fetch('https://anapioficeandfire.com/api/characters/1031')
+  .then(resp=>resp.json())
+  .then(json=>console.log(json))
+}
 
 function renderCharacters(json){
   debugger
