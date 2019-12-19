8bbd64329c9ec360a10fb830d30c836ddd4dda48
Yingqi Chen <chanwk18@hotmail.com>
2019-12-19 00:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
+    h2.innerHTML = `<h2>${book.numberOfPages}</h2>`
     main.appendChild(h2)
   })
 }
