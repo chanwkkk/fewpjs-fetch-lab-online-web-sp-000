52cce1488cc5f27a191414d8847f02097310e4a2
Yingqi Chen <chanwk18@hotmail.com>
2019-12-19 00:30:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ document.addEventListener('keydown', function() {
 })
 
 function fetchCharacter1031(){
-  return (
-    fetch('https://anapioficeandfire.com/api/characters/1031', {
-    mode: 'no-cors'
-  }))
+  return fetch('https://anapioficeandfire.com/api/characters/1031', {mode: 'no-cors'}
+  )
   .then(resp=>resp.json())
   .then(json=>console.log(json))
 }
